c346a555691037bdce10206546ce0c8a40950eaf
trujilml <mickeytrj@gmail.com>
2021-11-24 14:04:57 -0600

    added middleware and functions to handle animal creation

--- server.js
+++ server.js
@@ -1,18 +1,62 @@
+const fs = require('fs');
+const path = require('path');
+
 const { animals } = require('./data/animals');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+//parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+//parse incoming JSON data
+app.use(express.json());
+
+
 function findById(id, animalsArray) {
     const result = animalsArray.filter(animal => animal.id === id)[0];
     return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({animals: animalsArray}, null, 2)
+    );
+    return animal;
+}
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true;
+}
+
 app.post('/api/animals', (req, res) => {
-    //req.body is where our incoming content will be
-    console.log(req.body);
-    res.json(req.body);
+    //set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    //if any data in req.body is incorrect, send 400 error back
+    if(!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    } else {
+    //add animal to json file and animals array in this function
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+    }
 });
 
 app.get('/api/animals', (req,res) => {
