abb95caa27556ec0f132f22809a2ee9539517441
trujilml <mickeytrj@gmail.com>
2021-11-25 10:51:14 -0600

    implemented route to index.html file and to see the frontend work on the localhost site server

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.static('public'));
+
 //parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 //parse incoming JSON data
@@ -113,6 +115,10 @@ function filterByQuery(query, animalsArray) {
     }
     return filteredResults;
 }
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 
 app.listen(PORT, () => {
