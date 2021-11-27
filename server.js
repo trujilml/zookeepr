e44963cd43300a3dbd96edaa1ebd8b9d9021a89f
trujilml <mickeytrj@gmail.com>
2021-11-27 15:06:13 -0600

    rearranged routes to respective files in the routes directory - site still functions

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const path = require('path');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 const { animals } = require('./data/animals');
 const express = require('express');
@@ -13,55 +15,8 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 //parse incoming JSON data
 app.use(express.json());
-
-app.post('/api/animals', (req, res) => {
-    //set id based on what the next index of the array will be
-    req.body.id = animals.length.toString();
-
-    //if any data in req.body is incorrect, send 400 error back
-    if(!validateAnimal(req.body)) {
-        res.status(400).send('The animal is not properly formatted.');
-    } else {
-    //add animal to json file and animals array in this function
-    const animal = createNewAnimal(req.body, animals);
-    res.json(animal);
-    }
-});
-
-app.get('/api/animals', (req,res) => {
-    let results = animals;
-    if (req.query) {
-        results = filterByQuery(req.query, results);
-    }
-    res.json(results);
-});
-
-app.get('/api/animals/:id', (req, res) => {
-    const result = findById(req.params.id, animals);
-    if (result) {
-    res.json(result);
-    } else {
-        res.send(404);
-    }
-});
-
-
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-app.get('/animals', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/animals.html'));
-});
-
-app.get('/zookeepers', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
